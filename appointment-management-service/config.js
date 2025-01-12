c23666eb8db097c1b7d30176fd14e582abb6efe1
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2025-01-12 15:36:25 +0530

    Integrating mysql DB

--- appointment-management-service/config.js
+++ appointment-management-service/config.js
@@ -1,11 +1,11 @@
 // Read configuration from the environment
 
 // Datbase configruation
-const dbHost = '';
-const dbUser = '';
-const dbPassword = '';
-const dbName = '';
-const dbPort = '';
+const dbHost = process.env.CHOREO_APPOINTMENT_MANAGEMENT_MYSQL_HOSTNAME;
+const dbUser = process.env.CHOREO_APPOINTMENT_MANAGEMENT_MYSQL_USERNAME;
+const dbPassword = process.env.CHOREO_APPOINTMENT_MANAGEMENT_MYSQL_PASSWORD;
+const dbName = process.env.CHOREO_APPOINTMENT_MANAGEMENT_MYSQL_DATABASENAME;
+const dbPort = process.env.CHOREO_APPOINTMENT_MANAGEMENT_MYSQL_PORT;
 
 // Twilio configruation
 const twilioAccountSid = process.env.CHOREO_TWILIO_CONNECTION_ACCOUNT_SID;
