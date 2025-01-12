b7fe33bdc1f7ca48e76d4b82c009cd7ebcc2a3fd
Janaka Pushpakumara <bajpushpakumara@gmail.com>
2025-01-12 14:49:05 +0530

    Integrating User Manager Service Connection

--- appointment-management-service/config.js
+++ appointment-management-service/config.js
@@ -12,9 +12,12 @@ const twilioAccountSid = '';
 const twilioAuthToken = '';
 const twilioFromPhoneNumber = '';
 
-// User Manager configruation
-const userManagerServiceUrl = '';
-const userManagerAPIkey = '';
+// User Manager configuration
+const userManagerServiceUrl = process.env.CHOREO_USER_MANAGER_SERVICE_CONNECTION_SERVICEURL;
+const userManagerAPIkey = process.env.CHOREO_USER_MANAGER_SERVICE_CONNECTION_CHOREOAPIKEY;
+const consumerKey = process.env.CHOREO_USER_MANAGER_SERVICE_CONNECTION_CONSUMERKEY;
+const consumerSecret = process.env.CHOREO_USER_MANAGER_SERVICE_CONNECTION_CONSUMERSECRET;
+const tokenUrl = process.env.CHOREO_USER_MANAGER_SERVICE_CONNECTION_TOKENURL;
 
 module.exports = {
     dbHost,
@@ -26,5 +29,8 @@ module.exports = {
     twilioAuthToken,
     twilioFromPhoneNumber,
     userManagerServiceUrl,
-    userManagerAPIkey
+    userManagerAPIkey,
+    consumerKey,
+    consumerSecret,
+    tokenUrl
 };
--- appointment-management-service/user_manager.js
+++ appointment-management-service/user_manager.js
@@ -1,6 +1,17 @@
 const axios = require('axios');
 const configs = require('./config');
-const { userManagerServiceUrl, userManagerAPIkey } = configs;
+const { userManagerServiceUrl, userManagerAPIkey, consumerKey, consumerSecret, tokenUrl } = configs;
+
+const getOAuth2Token = async () => {
+    const credentials = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
+    const response = await axios.post(tokenUrl, 'grant_type=client_credentials', {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${credentials}`
+        }
+    });
+    return response.data.access_token;
+};
 
 const api = axios.create({
     baseURL: userManagerServiceUrl,
@@ -12,7 +23,12 @@ const api = axios.create({
 
 const getUserDetails = async (email) => {
     try {
-        const response = await api.get(`/users/${email}`);
+        const accessToken = await getOAuth2Token();
+        const response = await api.get(`/users/${email}`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
         console.log(response);
         return response.data;
     } catch (error) {
